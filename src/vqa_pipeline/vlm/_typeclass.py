a0b20e9793a71b7086271c1703bfa56ea2921802
J-joon <jjkim030309@gmail.com>
2025-09-05 16:48:58 +0900

    update _typeclass introducing generic protocol

--- src/vqa_pipeline/vlm/_typeclass.py
+++ src/vqa_pipeline/vlm/_typeclass.py
@@ -1,6 +1,8 @@
-from typing import Protocol, runtime_checkable
+from typing import Protocol, runtime_checkable, TypeVar
 from vqa_pipeline.image import ImageProvider
+
+T_Image = TypeVar("T_Image", bound=ImageProvider)
 
 @runtime_checkable
 class VLM(Protocol):
-    def question(self, images: list[ImageProvider], prompts: dict[str, str]) -> Result[dict[str, str], str]: ...
+    def question(self, images: list[T_Image], prompts: dict[str, str]) -> Result[dict[str, str], str]: ...

f7af317c6ffa108fef44a2073e2120a56c2a1ab7
J-joon <jjkim030309@gmail.com>
2025-09-09 15:16:49 +0900

    update __init__

--- src/vqa_pipeline/vlm/__init__.py
+++ src/vqa_pipeline/vlm/__init__.py
@@ -1,11 +1,24 @@
-from ._typeclass import VLM, BBoxProvider, T_Image
-from .vlms import InternVL3, ImageLabelProvider, 
-from .bbox_providers import GroundingDino,
+from ._typeclass import (
+    VLM,
+    BBoxProvider,
+    T_Image,
+)
+from .vlms import (
+    InternVL3,
+    ImageLabelProvider,
+)
+from .bbox_providers import (
+    GroundingDino,
+)
+from vqa_pipeline.image import (
+    ImageProvider,
+)
 
 __all__ = [
-        "VLM",
-        "BBoxProvider",
-        "ImageLabelProvider",
-        "InternVL3",
-        "GroundingDino",
-        ]
+    "VLM",
+    "BBoxProvider",
+    "ImageProvider",
+    "ImageLabelProvider",
+    "InternVL3",
+    "GroundingDino",
+]

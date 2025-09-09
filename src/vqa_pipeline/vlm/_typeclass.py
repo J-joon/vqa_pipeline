c367363aac4df8a878f9f1bf2b533069709f1d85
J-joon <jjkim030309@gmail.com>
2025-09-09 15:04:42 +0900

    create bbox_providers.py

--- src/vqa_pipeline/vlm/_typeclass.py
+++ src/vqa_pipeline/vlm/_typeclass.py
@@ -1,9 +1,14 @@
-from typing import Protocol, runtime_checkable, TypeVar
+from typing import Protocol, runtime_checkable, TypeVar, TypeAlias
 from vqa_pipeline.image import ImageProvider
-from static_error_handler import *
+from vqa_pipeline import Box
+from static_error_handler import Ok, Err, Result
 
 T_Image = TypeVar("T_Image", bound=ImageProvider)
 
 @runtime_checkable
 class VLM(Protocol):
     def question(self, images: list[T_Image], prompts: tuple[tuple[str, str],...]) -> Result[tuple[tuple[str, str], ...], str]: ...
+
+@runtime_checkable
+class BBoxProvider(Protocol):
+    def query(self, image: T_Image, query: str) -> Result[tuple[Box, ...], str]: ...
